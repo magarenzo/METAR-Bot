15488a0defa36439e44073ff51ec46b0b0b0d493
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2017-08-17 09:28:41 -0400

    removed unnecessary code; fixed csv writer

--- script.py
+++ script.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python
 
-import time
-import datetime as dateTime
 import urllib2
 import xml.etree.ElementTree as elementTree
 import csv
-
-localtime = time.localtime(time.time())
 
 # Download XML data
 url = urllib2.urlopen('http://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=KJFK&mostRecent=true&hoursBeforeNow=1')
 body = url.read()
 
-# Extract and parse XML data; Add data to copied template
+# Extract and parse XML data
 data = elementTree.fromstring(body)
 d = data.findall('data')
 
+# Append parsed XML data to existing CSV file
 for i in d:
-    for j in i:
-        hour = dateTime.datetime.now().hour
-        hour += 2
-        # Create a CSV writer object and write to the file
-        with open('data.csv', 'w') as csvfile:
-                dataWriter = csv.writer(csvfile, delimiter= ' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                dataWriter.writerow(j.find('observation_time').text)
-                dataWriter.writerow(j.find('temp_c').text)
+        for j in i:
+                with open('data.csv', 'a') as f:
+                        writer = csv.writer(f, delimiter=' ', quotechar='|')
+                        writer.writerow(j.find('observation_time').text)
+                        writer.writerow(j.find('temp_c').text)
