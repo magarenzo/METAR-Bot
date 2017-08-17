fcca867d57cf52e89be0763e84bdcfde92ce9ace
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2017-08-17 11:08:19 -0400

    checks if file exists; clean up parsed data

--- script.py
+++ script.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python
 
+import os.path
+import csv
 import urllib2
 import xml.etree.ElementTree as elementTree
-import csv
+import re
+
+path = './data.csv'
+
+# If file does not exist, create it and add the header
+value = os.path.exists(path)
+if (value == False):
+        f = open(path, 'w+')
+        with open(path, 'w') as f:
+                writer = csv.writer(f, delimiter=' ')
+                writer.writerow('date/time,degrees_celsius')
 
 # Download XML data
 url = urllib2.urlopen('http://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=KJFK&mostRecent=true&hoursBeforeNow=1')
 body = url.read()
 
-# Extract and parse XML data
+# Extract XML data
 data = elementTree.fromstring(body)
 d = data.findall('data')
 
-# Append parsed XML data to existing CSV file
+# Parse for wanted XML data, remove unwanted characters, and append to existing CSV file
 for i in d:
         for j in i:
-                with open('./data.csv', 'a') as f:
-                        writer = csv.writer(f, delimiter=' ', quotechar='|')
-                        writer.writerow(j.find('observation_time').text)
-                        writer.writerow(j.find('temp_c').text)
+                with open(path, 'a') as f:
+                        writer = csv.writer(f, delimiter=' ')
+                        time = j.find('observation_time').text
+                        time = re.sub('T', '-', time)
+                        time = re.sub('Z', '', time)
+                        temp = j.find('temp_c').text
+                        writer.writerow(time + ',' + temp)
